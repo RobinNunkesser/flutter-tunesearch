7167c67d60e9e6b4820a44d483bc820a2845f3be
Robin Nunkesser <robin.nunkesser@hshl.de>
2018-12-19 17:38:46 +0100

    Improved track sorting

--- tune_search/lib/track_entity.dart
+++ tune_search/lib/track_entity.dart
@@ -1,11 +1,18 @@
-class TrackEntity  implements Comparable {
+class TrackEntity implements Comparable {
   final String artistName;
   final String collectionName;
   final String trackName;
   final int trackNumber;
+  final int discNumber;
   final String artworkUrl60;
 
-  TrackEntity({this.artistName,this.collectionName,this.trackName,this.trackNumber,this.artworkUrl60});
+  TrackEntity(
+      {this.artistName,
+      this.collectionName,
+      this.trackName,
+      this.trackNumber,
+      this.discNumber,
+      this.artworkUrl60});
 
   factory TrackEntity.fromJson(Map<String, dynamic> json) {
     return TrackEntity(
@@ -13,6 +20,7 @@ class TrackEntity  implements Comparable {
       collectionName: json['collectionName'],
       trackName: json['trackName'],
       trackNumber: json['trackNumber'],
+      discNumber: json['discNumber'],
       artworkUrl60: json['artworkUrl60'],
     );
   }
@@ -21,10 +29,13 @@ class TrackEntity  implements Comparable {
   String toString() => '$trackNumber - $trackName';
 
   @override
-  int compareTo(other) {    
+  int compareTo(other) {
     if (other is TrackEntity) {
-      if (collectionName==other.collectionName) return trackNumber.compareTo(other.trackNumber);
-      return collectionName.compareTo(other.collectionName);
+      if (collectionName != other.collectionName)
+        return collectionName.compareTo(other.collectionName);
+      if (discNumber != other.discNumber)
+        return discNumber.compareTo(other.discNumber);
+      return trackNumber.compareTo(other.trackNumber);
     } else {
       return null;
     }
