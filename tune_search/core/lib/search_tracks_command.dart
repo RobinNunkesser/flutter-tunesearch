abd5a3bd2790049dca80a675870c48edd37d69ca
Robin Nunkesser <robin.nunkesser@hshl.de>
2020-10-28 19:50:45 +0100

    Add mock

--- tune_search/core/lib/search_tracks_command.dart
+++ tune_search/core/lib/search_tracks_command.dart
@@ -1,6 +1,13 @@
 import 'package:core_ports/abstract_search_tracks_command.dart';
+import 'package:core_ports/collection_entity.dart';
+import 'package:core_ports/search_tracks_dto.dart';
 
 class SearchTracksCommand implements AbstractSearchTracksCommand {
-  
+  @override
+  Future<List<CollectionEntity>> execute({SearchTracksDTO inDTO}) {
+    // TODO: implement execute
+    throw UnimplementedError();
+  }
+
 
 }
